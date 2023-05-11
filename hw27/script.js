edcd50d7dec757034f6743a73da33d8cff9380e5
Vadim Morvanyuk <vadim.morvanyuk@gmail.com>
2023-05-11 13:23:09 +0300

    add hw27

--- hw27/script.js
+++ hw27/script.js
@@ -2,21 +2,19 @@
 // яка приймає масив із чисел та масивів чисел (наприклад [1,2,3])
 //  і генерує список з елементів:
 const generateList = (array) => {
-  const main = document.querySelector(".main");
   const ul = document.createElement("ul");
+
   array.forEach((item) => {
     const li = document.createElement("li");
     if (typeof item === "number") {
       li.textContent = item;
-    } else if (typeof item === 'object') {
-      generateList(item);
+    } else if (typeof item === "object") {
+      li.appendChild(generateList(item));
     }
     ul.appendChild(li);
   });
 
-  main.appendChild(ul);
+  return ul;
 };
 
-generateList([1, 2, 3, [4, 5]]);
-
-
+document.body.appendChild(generateList([1, 2, 3, [4, 5, 6]]));
