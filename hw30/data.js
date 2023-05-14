28cd4712058531380b98c15ea57dfc1530edfc8d
Vadim Morvanyuk <vadim.morvanyuk@gmail.com>
2023-05-14 20:06:45 +0300

    add hw30

---
+++ hw30/data.js
@@ -0,0 +1,85 @@
+// const data = [
+//   {
+//     name: "Phones",
+//     products: [
+//       {
+//         name: "Iphone 13 Pro",
+//         description: "Iphone 13 Pro Description",
+//         price: 30000,
+//       },
+//       {
+//         name: "Iphone 14 Pro",
+//         description: "Iphone 14 Pro Description",
+//         price: 50000,
+//       },
+//       {
+//         name: "Iphone 14 Pro Max",
+//         description: "Iphone 14 Pro Max Description",
+//         price: 60000,
+//       },
+//     ],
+//   },
+//   {
+//     name: "Laptops",
+//     products: [
+//       {
+//         name: "Asus",
+//         description: "Asus Decription",
+//         price: 25000,
+//       },
+//       {
+//         name: "Acer",
+//         description: "Acer description",
+//         price: 30000,
+//       },
+//       {
+//         name: "Apple Macbook",
+//         description: "Apple Macbook Description",
+//         price: 70000,
+//       },
+//     ],
+//   }
+// ];
+
+const data = [
+  {
+    name: "Phones",
+    products: [
+      {
+        name: "Iphone 13 Pro",
+        description: "Iphone 13 Pro description",
+        price: 30000,
+      },
+      {
+        name: "Iphone 13 Pro Max",
+        description: "Iphone 13 Pro Max description",
+        price: 45000,
+      },
+      {
+        name: "Iphone 14 Pro Max",
+        description: "Iphone 14 Pro Max description",
+        price: 55000,
+      },
+    ],
+  },
+  {
+    name: "Laptops",
+    products: [
+      {
+        name: "Acer Aspire 7",
+        description: "Acer Aspire 7 description",
+        price: 30000,
+      },
+      {
+        name: "Apple Macbook",
+        description: "Apple Macbook description",
+        price: 50000,
+      },
+      {
+        name: "Asus",
+        description: "Asus description",
+        price: 20000,
+      },
+    ],
+  },
+];
---
+++ hw30/script.js
@@ -0,0 +1,192 @@
+let orders = [];
+
+function showCategories() {
+  const container = document.querySelector(".categories");
+  container.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    const el = document.createElement("div");
+    el.innerHTML = data[i].name;
+    el.setAttribute("data-category", i);
+    el.addEventListener("click", showProductsHandler);
+    container.appendChild(el);
+  }
+  const orders = document.createElement("button");
+
+  orders.addEventListener("click", showOrders);
+  orders.innerHTML = "My orders";
+  container.appendChild(orders);
+}
+function showOrders() {
+  const cancel = document.createElement("button");
+  cancel.classList.add("cancel");
+  cancel.innerHTML = "Cancel";
+  let myOrders = document.createElement("div");
+  let categories = document.querySelector(".categories");
+  categories.innerHTML = "";
+  myOrders.classList.add("myOrders");
+  const order = localStorage.getItem("myOrders");
+  orders = JSON.parse(order);
+  if (orders === null) {
+    myOrders.innerHTML = "There aren't any orders yet";
+  } else {
+    orders.forEach((item) => {
+      const order = document.createElement("p");
+      const deleteBtn = document.createElement("button");
+      const orderInfo = document.createElement("p");
+      orderInfo.innerHTML = `${item.name} ${item.amount} ${item.price} ${item.date}`;
+      deleteBtn.setAttribute("data-id", item.id);
+      let isVisible = false;
+      deleteBtn.classList.add("delete");
+      deleteBtn.innerHTML = "X";
+      order.setAttribute("data-id", item.id);
+      order.style.cursor = "pointer";
+      order.innerHTML = `${item.date} ${item.price}`;
+      order.appendChild(deleteBtn);
+      myOrders.appendChild(order);
+      order.addEventListener("click", () => {
+        if (isVisible == false) {
+          order.innerText = `${item.name} ${item.amount} ${item.price} ${item.date}`;
+          order.appendChild(deleteBtn);
+          isVisible = true;
+        } else {
+          order.innerText = `${item.date} ${item.price}`;
+          order.appendChild(deleteBtn);
+          isVisible = false;
+        }
+      });
+    });
+  }
+
+  categories.appendChild(myOrders);
+
+  categories.appendChild(cancel);
+  cancel.addEventListener("click", showCategories);
+  deleteOrders();
+}
+
+function deleteOrders() {
+  const btns = document.querySelectorAll(".delete");
+
+  btns.forEach((item) => {
+    item.addEventListener("click", (event) => {
+
+
+      
+      // HERE IS MISTAKE
+
+      const id = event.target.dataset.id;
+      console.log(orders, id);
+      orders.filter((f) => f.id != id);
+      localStorage.setItem("myOrders", JSON.stringify(orders));
+      showOrders();
+
+      // HERE IS MISTAKE
+
+      if (orders.length == 0) {
+        localStorage.removeItem("myOrders");
+        let categories = document.querySelector(".categories");
+        let myOrders = document.querySelector(".myOrders");
+        let cancel = document.querySelector(".cancel");
+
+        myOrders.innerHTML = "There aren't any orders yet";
+        categories.appendChild(myOrders);
+        categories.appendChild(cancel);
+      }
+    });
+  });
+}
+
+function showProductsHandler(event) {
+  const container = document.querySelector(".products");
+  container.innerHTML = "";
+  const el = event.target;
+  const categoryIndex = el.getAttribute("data-category");
+  const categoryProducts = data[categoryIndex].products;
+
+  for (let i = 0; i < categoryProducts.length; i++) {
+    const el = document.createElement("div");
+    el.innerHTML = categoryProducts[i].name;
+    el.setAttribute("data-product", categoryIndex);
+    el.setAttribute("data-category", i);
+    el.addEventListener("click", showDetails);
+    container.appendChild(el);
+  }
+}
+
+function showDetails(event) {
+  const el = event.target;
+  const container = document.querySelector(".details");
+  const categoryIndex = el.getAttribute("data-product");
+  const productIndex = el.getAttribute("data-category");
+  const productDetails = data[categoryIndex].products[productIndex];
+  const productName = document.createElement("div");
+  productName.classList.add("product-item");
+  const productDescription = document.createElement("div");
+  const productPrice = document.createElement("div");
+  const button = document.createElement("button");
+  productPrice.classList.add("price");
+  button.innerHTML = "Buy product!";
+  productName.innerHTML = productDetails.name;
+  productDescription.innerHTML = productDetails.description;
+  productPrice.innerHTML = productDetails.price;
+  container.innerHTML = "";
+
+  button.addEventListener("click", buyProduct);
+  container.append(productName, productDescription, productPrice, button);
+}
+
+function buyProduct(event) {
+  const form = document.forms.formOrder;
+  form.style.display = "block";
+
+  const button = document.getElementById("btn");
+
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    checkYourOrder();
+  });
+
+  form.appendChild(button);
+  let body = document.querySelector("body");
+}
+
+function checkYourOrder() {
+  const form = document.forms.formOrder;
+  const amountOfProduct = form.amount.value;
+  const address = form.address.value;
+  const products = document.querySelector(".products");
+  const details = document.querySelector(".details");
+  const product = document.querySelector(".product-item").textContent;
+  const childrens = [...form.children];
+  const price = document.querySelector(".price").textContent;
+  const lengthOfItems = [];
+  childrens.forEach((item) => {
+    lengthOfItems.push(item.value.length);
+  });
+  lengthOfItems.pop();
+  if (lengthOfItems.includes(0)) {
+    alert("Fill in all the information about your order!");
+  } else {
+    alert(
+      `You have bought ${amountOfProduct} ${product} \n Your address: ${address}`
+    );
+    if (orders == null) {
+      orders = [];
+    }
+    let itemID = orders.length + 1;
+    orders.push({
+      id: itemID,
+      name: product,
+      amount: amountOfProduct,
+      price: price,
+      date: `${new Date()}`,
+      address: address,
+    });
+    localStorage.setItem("myOrders", JSON.stringify(orders));
+
+    products.innerHTML = "";
+    details.innerHTML = "";
+    form.style.display = "none";
+  }
+}
+showCategories();
