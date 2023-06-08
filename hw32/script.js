2fd53a278037e281e2c75577c8d5593c5a367c84
Vadim Morvanyuk <vadim.morvanyuk@gmail.com>
2023-06-08 11:19:06 +0300

    fix hw32

--- hw32/script.js
+++ hw32/script.js
@@ -1,40 +1,37 @@
 const showTime = () => {
-  const container = document.querySelector(".container");
   const currentDate = new Date();
-  const hours = String(currentDate.getHours()).split("");
-  const minutes = String(currentDate.getMinutes()).split("");
-  const seconds = String(currentDate.getSeconds()).split("");
+  const hours = Math.floor(currentDate.getHours() / 10);
+  const hours2 = currentDate.getHours() % 10;
+  const minutes = Math.floor(currentDate.getMinutes() / 10);
+  const minutes2 = currentDate.getMinutes() % 10;
+  const seconds = Math.floor(currentDate.getSeconds() / 10);
+  const seconds2 = currentDate.getSeconds() % 10;
 
-  if (seconds[1] === undefined) {
-    seconds[1] = seconds[0];
-    seconds[0] = 0;
-  }
-  if (minutes[1] === undefined) {
-    minutes[1] = minutes[0];
-    minutes[0] = 0;
-  }
-  if (hours[1] === undefined) {
-    hours[1] = hours[0];
-    hours[0] = 0;
-  }
-  const sec1 = document.createElement("img");
-  const sec2 = document.createElement("img");
-  sec1.src = `./img/${seconds[0]}.png`;
-  sec2.src = `./img/${seconds[1]}.png`;
+  const changeSrc = (img, src) => {
+    if (img.src !== src) {
+      img.src = src;
+    }
+  };
 
-  const min1 = document.createElement("img");
-  const min2 = document.createElement("img");
-  min1.src = `./img/${minutes[0]}.png`;
-  min2.src = `./img/${minutes[1]}.png`;
+  const hour1 = document.querySelector(".hour1");
+  const hour2 = document.querySelector(".hour2");
 
-  const hour1 = document.createElement("img");
-  const hour2 = document.createElement("img");
-  hour1.src = `./img/${hours[0]}.png`;
-  hour2.src = `./img/${hours[1]}.png`;
-  const time = document.createElement("div");
-  time.append(hour1, hour2, min1, min2, sec1, sec2);
+  const min1 = document.querySelector(".min1");
+  const min2 = document.querySelector(".min2");
 
-  container.replaceChildren(time);
+  const sec1 = document.querySelector(".sec1");
+  const sec2 = document.querySelector(".sec2");
+
+  changeSrc(hour1, `./img/${hours}.png`);
+  changeSrc(hour2, `./img/${hours2}.png`);
+
+  changeSrc(min1, `./img/${minutes}.png`);
+  changeSrc(min2, `./img/${minutes2}.png`);
+
+  changeSrc(sec1, `./img/${seconds}.png`);
+  changeSrc(sec2, `./img/${seconds2}.png`);
+
+  setTimeout(showTime, 1000);
 };
 
-setInterval(showTime, 1000);
+showTime();
