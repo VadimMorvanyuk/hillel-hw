7241a385b3cd26dff4f42284aa09c9b64b89b5b1
Vadim Morvanyuk <vadim.morvanyuk@gmail.com>
2023-06-14 17:09:37 +0300

    refactor hw33

--- hw33/script.js
+++ hw33/script.js
@@ -1,16 +1,10 @@
-const p = [...document.getElementsByTagName("p")];
-const option = document.querySelector(".optionValue");
-let value = option.value;
-fetch(
-  `http://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&APPID=5d066958a60d315387d9492393935c19`
-)
-  .then((res) => {
-    return res.json();
-  })
-  .then((res) => {
-    p.forEach((item) => {
-      item.textContent = item.id;
-    });
+const option = document.querySelector('.optionValue');
+
+option.onchange = (event) => {
+  value = event.target.value;
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&APPID=5d066958a60d315387d9492393935c19`)
+  .then((res) => res.json())
+  .then(function (res) {
     const city = res.name;
     const temp = res.main.temp;
     const pressure = res.main.pressure;
@@ -19,39 +13,14 @@ fetch(
     const speed = res.wind.speed;
     const deg = res.wind.deg;
     const arr = [city, temp, pressure, description, humidity, speed, deg];
+
+    const p = [...document.getElementsByTagName("p")];
     arr.forEach((item, index) => {
-      p[index].textContent += ` ${item}`;
+      p[index].textContent = `${p[index].id} ${item}`;
     });
     const icon = document.getElementById("icon-weather");
     const iconID = res.weather[0].icon
     icon.src = `http://openweathermap.org/img/w/${iconID}.png`;
-    
-  });
-option.onchange = (event) => {
-  p.forEach((item) => {
-    item.textContent = item.id;
-  });
-  value = event.target.value;
-  fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&APPID=5d066958a60d315387d9492393935c19`
-  )
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (res) {
-      const city = res.name;
-      const temp = res.main.temp;
-      const pressure = res.main.pressure;
-      const description = res.weather[0].description;
-      const humidity = res.main.humidity;
-      const speed = res.wind.speed;
-      const deg = res.wind.deg;
-      const arr = [city, temp, pressure, description, humidity, speed, deg];
-      arr.forEach((item, index) => {
-        p[index].textContent += ` ${item}`;
-      });
-      const icon = document.getElementById("icon-weather");
-      const iconID = res.weather[0].icon
-    icon.src = `http://openweathermap.org/img/w/${iconID}.png`;
-    });
+  })
 };
+option.dispatchEvent(new Event('change'));
