5f9db5b8e09ef7ae6ccaab1da20d67ccd7b9c1b1
Vadim Morvanyuk <vadim.morvanyuk@gmail.com>
2023-07-15 18:02:29 +0300

    add hw39

--- hw39/my-app/src/App.js
+++ hw39/my-app/src/App.js
@@ -1,25 +1,72 @@
-import Example from "./Example";
 import React from "react";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      country: 'Ukraine'
+      posts: [],
     };
   }
 
-  changeNameHandler = () => {
-    const value = this.state.country === 'Ukraine' ? 'Ukraine is the best country' : 'Ukraine';
-    this.setState({ country: value });
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({ posts: json });
+      });
+  }
+
+  deletePostHandler = (event) => {
+    const postId = event.target.parentElement.id;
+    this.setState(
+      (prevState) => {
+        const updatedPosts = prevState.posts.filter(
+          (post) => post.id !== +postId
+        );
+        return { posts: updatedPosts };
+      },() => {
+        const postElement = document.getElementById(postId);
+        if (postElement) {
+          postElement.remove();
+        }
+      }
+    );
+  };
+
+  addPostHandler = () => {
+    const newPost = {
+      id: this.state.posts.length + 1,
+      title: "New Post",
+      body: "This is a new post.",
+    };
+
+    this.setState((prevState) => ({
+      posts: [newPost, ...prevState.posts],
+    }));
   };
 
   render() {
     return (
       <div className="App">
         <h1>App</h1>
-        <Example country={this.state.country} />
-        <button onClick={this.changeNameHandler}>Click</button>
+        <button onClick={this.addPostHandler} className="add">
+          Add post
+        </button>
+        {this.state.posts.map((item) => {
+          return (
+            <div className="post" id={item.id} key={item.id}>
+              <h3 contentEditable suppressContentEditableWarning>
+                {item.title}
+              </h3>
+              <p contentEditable suppressContentEditableWarning>
+                {item.body}
+              </p>
+              <button onClick={this.deletePostHandler} className="delete">
+                Delete post
+              </button>
+            </div>
+          );
+        })}
       </div>
     );
   }
